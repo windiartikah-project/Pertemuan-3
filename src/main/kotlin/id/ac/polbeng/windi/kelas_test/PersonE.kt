8bb80d501378030f0c42be34df36af470f3a3859
windiartikah <windiartikah25@gmail.com>
2022-03-25 14:34:58 +0700

    percobaan class dan objek

---
+++ src/main/kotlin/id/ac/polbeng/windi/kelas_test/PersonE.kt
@@ -0,0 +1,30 @@
+package id.ac.polbeng.windi.kelas_test
+
+class PersonE {
+    var firstName: String
+    var lastName: String
+    var age: Int = 25
+    // this(_firstName, _lastName) akan dipanggil terlebih dahulu
+    constructor(_firstName: String, _lastName: String){
+        firstName = _firstName
+        lastName = _lastName
+    }
+
+    //kode dieksekusi.
+    constructor(_firstName: String, _lastName: String, _age: Int): this(_firstName, _lastName){
+        age = _age
+    }
+    fun cetakInfo(){
+        println("Name : ${firstName} ${lastName}")
+        println("Age : ${age}")
+    }
+}
+
+fun main(){
+    val anton = PersonE("Frank", "Lampard")
+    anton.cetakInfo()
+    println()
+    val budi = PersonE("Budi", "Gunawan", 21)
+    budi.cetakInfo()
+
+}
