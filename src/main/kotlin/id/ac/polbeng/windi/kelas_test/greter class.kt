03a2f4852474e2c9a99cc1bb0cc4c26b16d81d47
windiartikah <windiartikah25@gmail.com>
2022-03-25 11:23:37 +0700

    greter.kt

---
+++ src/main/kotlin/id/ac/polbeng/windi/kelas_test/greter class.kt
@@ -0,0 +1,8 @@
+package id.ac.polbeng.windi.kelas_test
+class Greeter {
+    fun greet() {
+        println("Hello object world!")
+    } }
+
+
+
---
+++ src/main/kotlin/id/ac/polbeng/windi/kelas_test/greter.kt
@@ -0,0 +1,7 @@
+package id.ac.polbeng.windi.kelas_test
+class greter {
+    fun greter () {
+        println("Hello object world!")
+    } }
+
+
