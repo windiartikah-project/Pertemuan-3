48d4521cb5e8b71ab0cd974864a5eca8dd66acad
windiartikah <windiartikah25@gmail.com>
2022-03-25 14:45:16 +0700

    percobaan class dan objek2

---
+++ src/main/kotlin/id/ac/polbeng/windi/kelas_test/percobaan kelas dan object2.kt
@@ -0,0 +1,15 @@
+package id.ac.polbeng.windi.kelas_test
+
+
+class Greeter1 {
+    var text: String = ""
+    fun greet() {
+        println("Hello object world!")
+    }
+    fun greet(name: String) {
+        println("$text $name")
+    }
+    fun with_ret_val(name: String): String {
+        return "$text $name"
+    }
+}
