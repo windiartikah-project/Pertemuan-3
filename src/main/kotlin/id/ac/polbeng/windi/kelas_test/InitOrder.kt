7c092f9138d811161caf503c36a1a39bafcc9ba0
windiartikah <windiartikah25@gmail.com>
2022-03-25 14:25:39 +0700

    percobaan class dan objek

---
+++ src/main/kotlin/id/ac/polbeng/windi/kelas_test/InitOrder.kt
@@ -0,0 +1,16 @@
+package id.ac.polbeng.windi.kelas_test
+
+class InitOrder(name: String) {
+    val firstProperty = "First property: $name".also(::println)
+    init {
+        println("First initializer block that prints ${name}")
+    }
+    val secondProperty = "Second property: ${name.length}".also(::println)
+    init {
+        println("Second initializer block that prints ${name.length}")
+    }
+}
+
+fun main(){
+    val initOrder = InitOrder("RPL")
+}
