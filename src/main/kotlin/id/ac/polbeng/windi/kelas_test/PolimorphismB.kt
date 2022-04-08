6e4d964b574f42169fb8631d1a6c789241eb21e5
windiartikah <windiartikah25@gmail.com>
2022-04-08 13:01:44 +0700

    Percobaan Polimorphism (Dynamic Polymorphism (Overriding Function))

---
+++ src/main/kotlin/id/ac/polbeng/windi/kelas_test/PolimorphismB.kt
@@ -0,0 +1,22 @@
+package id.ac.polbeng.windi.kelas_test
+
+//dilakukan pengikatan Run-Time
+//Dalam kelas yang berbeda
+open class MyBase {
+    // Must use "open" modifier to allow child classes to override it
+    open fun think() {
+        println("Hey!! i am thinking ")
+    }
+}
+class MyDerived: MyBase() { // inheritance happens using default constructor
+    // Must use "override" modifier to override a base class function
+    override fun think() {
+        println("I Am from Child")
+    }
+}
+fun main() {
+    val myBase = MyBase()
+    myBase.think()
+    var myDerived = MyDerived()
+    myDerived.think()
+}
