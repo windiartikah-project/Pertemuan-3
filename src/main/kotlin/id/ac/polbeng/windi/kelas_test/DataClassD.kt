f20f3d916d54a405110241a894573e1e96c223dd
windiartikah <windiartikah25@gmail.com>
2022-04-08 14:08:03 +0700

    Percobaan Data Kelas (Data class toString() method )

---
+++ src/main/kotlin/id/ac/polbeng/windi/kelas_test/DataClassD.kt
@@ -0,0 +1,8 @@
+package id.ac.polbeng.windi.kelas_test
+
+
+fun main() {
+    val meri = Student("Meri", 20)
+    //Fungsi toString() pada data kelas mengembalikan nilai kembalian String yang merupakan representasi dari objek
+    println(meri.toString())
+}
