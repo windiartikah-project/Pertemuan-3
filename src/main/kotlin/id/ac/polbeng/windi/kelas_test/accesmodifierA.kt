9c89697ee43a3aa8b3fb18323f151a05caab3686
windiartikah <windiartikah25@gmail.com>
2022-04-08 13:13:14 +0700

    Percobaan Access Modifier

---
+++ src/main/kotlin/id/ac/polbeng/windi/kelas_test/accesmodifierA.kt
@@ -0,0 +1,38 @@
+package id.ac.polbeng.windi.kelas_test
+
+open class Mahasiswa(var nama: String, var nrp: Int){
+    open fun info() {
+        println(nama +'\n'+ nrp);
+    }
+
+    //Tambahkan override agar tidak eror
+    override fun toString(): String {
+        return "Mahasiswa{nama= $nama, nrp= $nrp}"
+    }
+}
+
+class KetuaHima(nama: String, nrp: Int, val jurusan: String) : Mahasiswa(nama, nrp) {
+
+    @Override
+    override fun info(){
+        println(nama +'\n'+ nrp +'\n'+ jurusan);
+    }
+}
+
+fun main(){
+    val budi = Mahasiswa("Budi Gunawan", 1106123)
+    val anton = KetuaHima("Anton", 1106789, "Teknik Informatika")
+    println(anton.toString());
+
+    println()
+    anton.info()
+    println("Jenis Kelas = " + anton.javaClass.simpleName)
+
+    println()
+    budi.info()
+    println("Jenis Kelas = " + budi.javaClass.simpleName)
+    val ucok = KetuaHima("Ucok", 1105239, "Teknik Elektro")
+    println()
+    ucok.info()
+    println("Jenis Kelas = " + ucok.javaClass.simpleName)
+}
